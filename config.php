df406b968926e5aac9767ef4f2a78108e8b76106
NPFON.COM <NPFON.COM@DESKTOP-3UH36N3>
2016-09-04 10:45:52 +0700

    Merge origin/master

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
-
+$query=mysql_connect("localhost","npfoncom_management","ZGUjGR6sw9");
+mysql_select_db("npfoncom_management",$query);
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
